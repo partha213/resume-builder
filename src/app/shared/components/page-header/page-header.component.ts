2aea79adc5c0c2358411e00bcc4f2297514c0d6e
partha213 <60566029+partha213@users.noreply.github.com>
2024-09-14 19:01:48 +0530

    pdf download

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-page-header',
@@ -13,4 +15,18 @@ export class PageHeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  downloadResume() {
+    const data = document.getElementById('resume-canvas');
+    if (data) {
+      html2canvas(data).then(canvas => {
+        const imgWidth = 208;
+        const imgHeight = canvas.height * imgWidth / canvas.width;
+        const contentDataURL = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        const position = 0;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+        pdf.save('Resume.pdf');
+      });
+    }
+  }
 }
